b4685d362dabb818503162c65f87ae2d6b56040f
dzonsi <nikola86_v@yahoo.com>
2020-01-21 17:32:11 +0100

    add new component HomeSection.vue in home-section

---
+++ src/components/home-section/HomeSection.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hero">
+    <HeroTitle></HeroTitle>
+    <HeroClients></HeroClients>
+    <HeroEarth></HeroEarth>
+  </div>
+</template>
+
+<script>
+import HeroTitle from '@/components/home-section/HeroTitle.vue'
+import HeroEarth from '@/components/home-section/HeroEarth.vue'
+import HeroClients from '@/components/home-section/HeroClients.vue'
+
+export default {
+  name: 'HomeSection',
+  components: {
+    HeroTitle,
+    HeroEarth,
+    HeroClients
+  }
+}
+</script>
+
+<style lang="scss">
+.hero {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+@include media-breakpoint-down(md) {
+  .hero {
+    background-color: #070707;
+  }
+}
+</style>
