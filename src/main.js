22af6a53b6dac6a3be831cd4dff35e65d071dc12
dzonsi <nikola86_v@yahoo.com>
2020-02-21 18:45:12 +0100

    uninstall unnecessary vue-spinners library

--- src/main.js
+++ src/main.js
@@ -10,10 +10,6 @@ import 'vue-awesome/icons'
 import Icon from 'vue-awesome/components/Icon'
 Vue.component('v-icon', Icon)
 
-// loading spinners
-import { VueSpinners } from '@saeris/vue-spinners'
-Vue.use(VueSpinners)
-
 // vue-fullpage
 import VueFullPage from 'vue-fullpage.js'
 Vue.use(VueFullPage)
